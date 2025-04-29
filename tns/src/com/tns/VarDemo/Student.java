71496b7af336ffaed6711f7e3eda2971ad10ca4e
Rishika6423 <22d41a0563@gmail.com>
2025-04-29 16:25:56 +0530

    static variable

---
+++ tns/src/com/tns/VarDemo/Student.java
@@ -0,0 +1,11 @@
+package com.tns.VarDemo;
+
+public class Student {
+	int sid;
+	String sname;
+	static String collegename="Sri Indu";
+	 void display() {
+		 System.out.println("The details of the student"+sid+","+sname+","+collegename);
+	 }
+
+}
