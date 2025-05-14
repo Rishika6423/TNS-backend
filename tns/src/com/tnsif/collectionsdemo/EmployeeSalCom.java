a6cc03df9dd9ad086a055c6206655f23199b303d
Rishika6423 <22d41a0563@gmail.com>
2025-05-14 17:46:23 +0530

    Comparator

--- tns/src/com/tnsif/collectionsdemo/EmployeeSalCom.java
+++ tns/src/com/tnsif/collectionsdemo/EmployeeSalCom.java
@@ -6,6 +6,11 @@ public class EmployeeSalCom implements Comparator<Employee> {
 
 	@Override
 	public int compare(Employee e1, Employee e2) {
-		return Double.compare(e2.sal,e1.sal);	}
-
+			if(e1.sal==e2.sal)
+			return 0 ;
+			else if(e1.sal>e2.sal)
+				return 1;
+			else
+				return -1;
+		}
 }
