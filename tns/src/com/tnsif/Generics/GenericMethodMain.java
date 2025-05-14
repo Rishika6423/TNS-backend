727f083e5c93805b95d41fe408cc6a87df4366b9
Rishika6423 <22d41a0563@gmail.com>
2025-05-14 17:47:31 +0530

    Generics

---
+++ tns/src/com/tnsif/Generics/GenericMethodMain.java
@@ -0,0 +1,14 @@
+package com.tnsif.Generics;
+
+public class GenericMethodMain {
+
+	public static void main(String[] args) {
+		Integer[] intarray= {1,2,3};
+		String[] stringarray= {"Allen","Dolly","Candy"};
+		
+		//call the method
+		GenericMethodDemo.displayArray(intarray);
+		GenericMethodDemo.displayArray(stringarray);
+	}
+
+}
